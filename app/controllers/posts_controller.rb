c0c26200f270767983746ccc9f0c0e52a1c0b53f
Devan Kestel <devan.kestel@gmail.com>
2015-11-10 02:11:09 -0500

    Guest users can post and logout.
    Still need to work on some styles with bootstrap.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,31 @@ class PostsController < ApplicationController
   respond_to :json, :html
 
   def index
-    respond_with Post.all    
+    if session[:guest_id]
+      respond_with User.find(session[:guest_id]).posts.all
+    else
+      respond_with sample_posts
+    end    
   end
   
   def show
     respond_with Post.find(params[:id])
   end
 
+  def logout
+    User.find(session[:guest_id]).posts.destroy_all
+    session[:guest_id] = nil
+    redirect_to '/'
+  end
+
   def create
-    respond_with Post.create(post_params)
+    if session[:guest_id]
+      respond_with User.find(session[:guest_id]).posts.create!(post_params)
+    else
+      @current_user = User.create()
+      session[:guest_id] = @current_user.id
+      respond_with @current_user.posts.create!(post_params)
+    end
   end
 
   def update
@@ -23,5 +39,13 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:id, :title, :content)
+  end
+  def add_sample_posts
+    sample_posts.each do |sample_post|
+      Post.create!(sample_post)
+    end
+  end
+  def sample_posts
+    [{"title": "Here Comes Honey Boo Boo", "content": "I got my go-go juice."},{"title": "Another hum-drum post.", "content": "Oh here we go again."}]
   end
 end
